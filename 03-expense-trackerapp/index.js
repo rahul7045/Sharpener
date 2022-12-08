93ecaad5e3970b4666ad908b87374ba3dab09b3b
rahul7045 <rahulpujari525@gmail.com>
2022-12-08 16:39:13 +0530

    expense tracker app

--- 03-expense-trackerapp/index.js
+++ 03-expense-trackerapp/index.js
@@ -11,15 +11,20 @@ const categoryEl = document.querySelector("#category");
 formEl.addEventListener('submit' , onSubmit)
 
 window.addEventListener('DOMContentLoaded' , function(){
-    for(let i=0;i<localStorage.length;i++){
-        const info = localStorage.getItem(localStorage.key(i));
-        const myinfo = JSON.parse(info) ;
-        showNewUserOnScreen(myinfo);
-    }
+    getDataFromApi();
 })
 
+function getDataFromApi(){
+    axios.get('https://crudcrud.com/api/cdbd5c3d2497440bbbf022cd7f58cabd/expensetracker')
+    .then(res =>{
+        for(var i=0;i<res.data.length;i++){
+            showNewUserOnScreen(res.data[i]);
+        }
+    })
+}
+
 function showNewUserOnScreen(myobj){
-   const child = `<li  id =${myobj.description}>Category :-${myobj.category} Amount:-${myobj.amount} Description:-${myobj.description} <button type="button" class="btn btn-danger btn-sm m-2 float-right" onClick = deleteItem("${myobj.description}")>Delete</button><button type="button" class="btn btn-primary m-2 float-right btn-sm" onClick = editItem('${myobj.description}','${myobj.category}','${myobj.amount}')>Edit</button></li>`
+   const child = `<li  id =${myobj._id}>Category :-${myobj.category} Amount:-${myobj.amount} Description:-${myobj.description} <button type="button" class="btn btn-danger btn-sm m-2 float-right" onClick = deleteItem("${myobj._id}")>Delete</button><button type="button" class="btn btn-primary m-2 float-right btn-sm" onClick = editItem('${myobj.description}','${myobj.category}','${myobj.amount}','${myobj._id}')>Edit</button></li>`
    userList.innerHTML = userList.innerHTML + child
 }
 
@@ -36,25 +41,39 @@ function onSubmit(e){
             description : descriptionEl.value,
             category : categoryEl.value
         }
-        localStorage.setItem(`user${descriptionEl.value}` , JSON.stringify(myobj) );
-        showNewUserOnScreen(myobj)
-        amountEl.value="";
-        descriptionEl.value =""
+
+        axios.post('https://crudcrud.com/api/cdbd5c3d2497440bbbf022cd7f58cabd/expensetracker',myobj)
+        .then(res =>{
+            userList.innerHTML ="";
+            getDataFromApi();
+        })
+        .catch(err =>console.log(err))
+       // localStorage.setItem(`user${descriptionEl.value}` , JSON.stringify(myobj) );
+       // showNewUserOnScreen(myobj)
+       amountEl.value="";
+       descriptionEl.value =""
     }
     
 }
 
-function deleteItem(description){
-    localStorage.removeItem(`user${description}`);
-    const child = document.getElementById(description);
+function deleteItem(id){
+    //localStorage.removeItem(`user${description}`);
+    //const child = document.getElementById(description);
 
-    userList.removeChild(child);
+    //userList.removeChild(child);
+    axios.delete(`https://crudcrud.com/api/cdbd5c3d2497440bbbf022cd7f58cabd/expensetracker/${id}`)
+        .then(res =>{
+            userList.innerHTML ="";
+            getDataFromApi();
+        })
+        .catch(err =>console.log(err))
+    
 }
 
-function editItem(description , category  , amount){
+function editItem(description , category  , amount , id){
     descriptionEl.value = description;
     amountEl.value = amount;
     categoryEl.value=category;
 
-    deleteItem(description);
+    deleteItem(id);
 }
