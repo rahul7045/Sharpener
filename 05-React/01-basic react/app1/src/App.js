7ea5cf4ab6ddb9b8feb7400af80ca89e832f2105
rahul7045 <rahulpujari525@gmail.com>
2022-12-13 11:31:10 +0530

    react first component

---
+++ 05-React/01-basic react/app1/src/App.js
@@ -0,0 +1,12 @@
+import ExpensesItem from "./components/ExpensesItem";
+function App() {
+  return (
+    <>
+      <h1>Lets get Started</h1>
+      <ExpensesItem />
+    </>
+
+  );
+}
+
+export default App;
---
+++ 05-React/01-basic react/app1/src/components/ExpensesItem.js
@@ -0,0 +1,10 @@
+function ExpensesItem(){
+    return <>
+      <h3>Expense Items</h3>
+      <p>Food Rs 10</p>
+      <p>Movies Rs 20</p>
+      <p>Petrol Rs 50</p>
+    </>
+}
+
+export default ExpensesItem;
