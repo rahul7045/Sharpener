27bb9aea9b9d7ff990b329b87d47b59e48c0b495
rahul7045 <rahulpujari525@gmail.com>
2022-12-13 13:12:54 +0530

    added expensedetail component

---
+++ 05-React/01-basic react/app1/src/components/ExpenseDetail.js
@@ -0,0 +1,11 @@
+function ExpenseDetail(props){
+   return (
+    <div className='expense-item__description'>
+        <h2>{props.title}</h2>
+        <div>{props.location}</div>
+        <div className='expense-item__price'>Rs{props.amount}</div>
+      </div>
+   )
+}
+
+export default ExpenseDetail
--- 05-React/01-basic react/app1/src/components/ExpensesItem.js
+++ 05-React/01-basic react/app1/src/components/ExpensesItem.js
@@ -1,18 +1,17 @@
 import './ExpensesItem.css'
+import ExpenseDate from './ExpenseDate';
+import ExpenseDetail from './ExpenseDetail';
 
 function ExpensesItem(props){
     // const expenseDate =new Date();
     // const expenseTitle = 'Car Insurance';
     // const expenseAmount = 294.67
-     const locationOfExpenditure = 'Mumbai'
+    // const locationOfExpenditure = 'Mumbai'
+   
     return(
      <div className='expense-item'>
-      <div>{props.date.toISOString()}</div>
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <div>{props.location}</div>
-        <div className='expense-item__price'>Rs{props.amount}</div>
-      </div>
+     <ExpenseDate date={props.date} />
+    <ExpenseDetail title={props.title} amount={props.amount} location={props.location} />s
     </div>
     );
 }
