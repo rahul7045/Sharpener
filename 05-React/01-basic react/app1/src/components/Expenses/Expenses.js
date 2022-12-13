c63ca218f670d92f850538cc03ab961a5c816ee1
rahul7045 <rahulpujari525@gmail.com>
2022-12-14 00:02:42 +0530

    expense list component and edge case

--- 05-React/01-basic react/app1/src/components/Expenses/Expenses.js
+++ 05-React/01-basic react/app1/src/components/Expenses/Expenses.js
@@ -3,6 +3,7 @@ import ExpensesItem from "./ExpensesItem"
 import './Expenses.css'
 import Card from "../UI/Card"
 import ExpenseFilter from "./ExpenseFilter"
+import ExpensesList from "./ExpensesList"
 const Expenses =(props)=>{
 
    const[filteredYear , setFilteredYear] = useState('2020')
@@ -12,16 +13,19 @@ const Expenses =(props)=>{
         setFilteredYear(selectedYear)   
     }
 
-    const filteredExpenses = props.data.filter(expenses=>expenses.date.getFullYear().toString() == filteredYear )
+    const filteredExpenses = props.data.filter(expenses=>expenses.date.getFullYear().toString() === filteredYear )
 
+
+ 
     return <Card className="expenses">
         <ExpenseFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
      {/* <ExpensesItem title={props.data[0].title} amount={props.data[0].amount} date={props.data[0].date} location={props.data[0].location}/> 
      <ExpensesItem title={props.data[1].title} amount={props.data[1].amount} date={props.data[1].date} location={props.data[1].location} />
      <ExpensesItem title={props.data[2].title} amount={props.data[2].amount} date={props.data[2].date} location={props.data[2].location} /> 
      
+     
      <ExpensesItem title={props.data[3].title} amount={props.data[3].amount} date={props.data[3].date} location={props.data[3].location} /> */}
-      {filteredExpenses.map(data1=><ExpensesItem key={data1.id} title={data1.title} amount={data1.amount} date={data1.date} />)}
+     <ExpensesList items={filteredExpenses} />
     </Card>   
 }
   
