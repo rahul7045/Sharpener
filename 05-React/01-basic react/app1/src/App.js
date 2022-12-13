8d33e9c12e782eaee3c7b0789c284bfb1d324cd6
rahul7045 <rahulpujari525@gmail.com>
2022-12-13 12:31:46 +0530

    passed data via props

--- 05-React/01-basic react/app1/src/App.js
+++ 05-React/01-basic react/app1/src/App.js
@@ -1,9 +1,27 @@
 import ExpensesItem from "./components/ExpensesItem";
 function App() {
+
+  const data = [
+    {id:'e1' ,title:'Car Insurance' , amount : 250 , date : new Date(2021,2,28) ,location:'Mumbai' },
+    {id:'e2' ,title:'Bike Insurance' , amount : 150 , date : new Date(2022,3,28) ,location:'Gurgaon'},
+    {id:'e3' ,title:'Truck Insurance' , amount : 350 , date : new Date(2019,5,20),location:'Bangalore' },
+    {id:'e4' ,title:'Home Insurance' , amount : 500 , date : new Date(2000,5,19),location:'Delhi' }
+
+  ]
+  // let innerdata =""
+  // const ele = data.forEach(info =>{
+  //   innerdata = innerdata + `<ExpensesItem title={info.title} amount={info.amount} date={info.date} />`
+  // })
+
   return (
     <>
       <h1>Lets get Started</h1>
-      <ExpensesItem />
+      <ExpensesItem title={data[0].title} amount={data[0].amount} date={data[0].date} location={data[0].location}/> 
+      <ExpensesItem title={data[1].title} amount={data[1].amount} date={data[1].date} location={data[1].location} /> 
+      <ExpensesItem title={data[2].title} amount={data[2].amount} date={data[2].date} location={data[2].location} /> 
+      <ExpensesItem title={data[3].title} amount={data[3].amount} date={data[3].date} location={data[3].location} /> 
+
+     {/* {innerdata} */}
     </>
 
   );
--- 05-React/01-basic react/app1/src/components/ExpensesItem.js
+++ 05-React/01-basic react/app1/src/components/ExpensesItem.js
@@ -1,17 +1,17 @@
 import './ExpensesItem.css'
 
-function ExpensesItem(){
-    const expenseDate =new Date();
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.67
-    const locationOfExpenditure = 'Mumbai'
+function ExpensesItem(props){
+    // const expenseDate =new Date();
+    // const expenseTitle = 'Car Insurance';
+    // const expenseAmount = 294.67
+     const locationOfExpenditure = 'Mumbai'
     return(
      <div className='expense-item'>
-      <div>{expenseDate.toISOString()}</div>
+      <div>{props.date.toISOString()}</div>
       <div className='expense-item__description'>
-        <h2>{expenseTitle}</h2>
-        <div>{locationOfExpenditure}</div>
-        <div className='expense-item__price'>Rs{expenseAmount}</div>
+        <h2>{props.title}</h2>
+        <div>{props.location}</div>
+        <div className='expense-item__price'>Rs{props.amount}</div>
       </div>
     </div>
     );
