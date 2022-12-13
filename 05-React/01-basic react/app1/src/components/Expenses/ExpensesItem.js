24c24b6b8874b0e7b0955afd679433d67ae48754
rahul7045 <rahulpujari525@gmail.com>
2022-12-13 15:00:21 +0530

    added delete button without functionality

--- 05-React/01-basic react/app1/src/components/Expenses/ExpensesItem.js
+++ 05-React/01-basic react/app1/src/components/Expenses/ExpensesItem.js
@@ -4,15 +4,23 @@ import ExpenseDetail from './ExpenseDetail';
 import Card from '../UI/Card'
 
 const ExpensesItem=(props)=>{
-    // const expenseDate =new Date();
-    // const expenseTitle = 'Car Insurance';
-    // const expenseAmount = 294.67
-    // const locationOfExpenditure = 'Mumbai'
+     let title = props.title
+    const changeTitleHandler = () =>{
+      title = 'Updated'
+      console.log(title)
+    }
+
+    const deleteExpenseHandler =()=>{
+        console.log('deleted')
+    }
    
     return(
      <Card className='expense-item'>
         <ExpenseDate date={props.date} />
-        <ExpenseDetail title={props.title} amount={props.amount} location={props.location} />s
+        <ExpenseDetail title={title} amount={props.amount} location={props.location} />
+        <button onClick ={changeTitleHandler}>Change Title</button>
+        <button onClick ={deleteExpenseHandler}>Delete</button>
+
     </Card>
     );
 }
