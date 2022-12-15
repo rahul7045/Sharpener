84c511c2fe30f7a2e07649f3d70477528fc5c61a
rahul7045 <rahulpujari525@gmail.com>
2022-12-15 20:21:55 +0530

    added card or meal summary

--- 05-React/04-restaurant-website/src/App.js
+++ 05-React/04-restaurant-website/src/App.js
@@ -1,9 +1,11 @@
 import React from "react";
 import Header from "./Components/Layout/Header";
+import Card from "./Components/UI/Card";
 function App() {
   return (
    <>
      <Header />
+     <Card />
    </>
   )
 }
