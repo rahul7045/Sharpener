d4cc358dcc0080d042b669c72c35a5b833f60813
rahul7045 <rahulpujari525@gmail.com>
2022-12-16 14:44:47 +0530

    createPortal cart

--- 05-React/04-restaurant-website/src/App.js
+++ 05-React/04-restaurant-website/src/App.js
@@ -2,9 +2,11 @@ import React from "react";
 import Header from "./Components/Layout/Header";
 import Card from "./Components/UI/Card";
 import Meals from "./Components/Meal/Meals";
+import Cart from "./Components/Cart/Cart";
 function App() {
   return (
    <>
+   <Cart />
      <Header />
      <Card />
      <Meals />
