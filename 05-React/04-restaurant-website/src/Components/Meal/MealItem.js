021c9db83fa48dd18ddfd01ddefb9105c9c93bbd
rahul7045 <rahulpujari525@gmail.com>
2022-12-16 13:50:28 +0530

    added mealbutton

--- 05-React/04-restaurant-website/src/Components/Meal/MealItem.js
+++ 05-React/04-restaurant-website/src/Components/Meal/MealItem.js
@@ -3,10 +3,16 @@ import './MealItem.css'
 
 const MealItem = (props) => {
   return (
+    <div className='flex-container'>
     <div className='mealitem card-body'>
         <h4 className='item-name'>{props.name}</h4>
         <h6 className='item-desc'>{props.desc}</h6>
         <h3 className='item-price'>{`$ ${props.price}`}</h3>
+    </div>
+    <div className='container-add-item'>
+      <div><label className='label-meal'>Amount</label><input className='input-meal'  type="number"/></div>
+      <button className='add-button-meal'>+ Add</button>
+    </div>
     </div>
   )
 }
--- 05-React/04-restaurant-website/src/Components/Meal/Meals.js
+++ 05-React/04-restaurant-website/src/Components/Meal/Meals.js
@@ -4,14 +4,14 @@ import './Meals.css'
 
  const Meals = () => {
     const Dummy_items =[
-        {name : "Sushi" , price: 12 , desc:"Finesh Fish and Veggies"},
-        {name : "Sushi" , price: 12 , desc:"Finesh Fish and Veggies"},
-        {name : "Sushi" , price: 12 , desc:"Finesh Fish and Veggies"},
-        {name : "Sushi" , price: 12 , desc:"Finesh Fish and Veggies"},
+        {id:1,name : "Sushi" , price: 12 , desc:"Finesh Fish and Veggies"},
+        {id:2,name : "Sushi" , price: 12 , desc:"Finesh Fish and Veggies"},
+        {id:3,name : "Sushi" , price: 12 , desc:"Finesh Fish and Veggies"},
+        {id:4,name : "Sushi" , price: 12 , desc:"Finesh Fish and Veggies"},
     ]
   return (
     <div className='card meal'>
-       {Dummy_items.map(data1=><MealItem  name={data1.name} price={data1.price} desc={data1.desc} />)}
+       {Dummy_items.map(data1=><MealItem key={data1.id}  name={data1.name} price={data1.price} desc={data1.desc} />)}
     </div>
   )
 }
