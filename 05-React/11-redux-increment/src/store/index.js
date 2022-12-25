3b4190cd09abe95e5f4ec35db5fd973ba2d9e6f6
rahul7045 <rahulpujari525@gmail.com>
2022-12-25 11:48:13 +0530

    used useSelector and useDispatch in component

--- 05-React/11-redux-increment/src/store/index.js
+++ 05-React/11-redux-increment/src/store/index.js
@@ -1,21 +1,21 @@
-import redux from 'redux'
+import {createStore} from 'redux'
 
 const counterReducer=(state={counter:0} , action)=>{
     if(action.type === "increment"){
       return{
-        counter : state.counter+2
+        counter : state.counter+5
       }
     }
     if(action.type === "decrement"){
         return{
-          counter : state.counter-2
+          counter : state.counter-5
         }
       }
 
       return state
 }
 
-const store = redux.createStore(counterReducer);
+const store = createStore(counterReducer);
 
 
 export default store
